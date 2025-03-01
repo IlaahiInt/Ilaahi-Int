9d26b52144ed92592ddc0169747387395a4988cc
IlaahiInt <ilaahiinternationaldigital@gmail.com>
2025-03-01 16:57:20 +0500

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -153,7 +153,7 @@ function App() {
     {
       name: "Dr. Ikhlaq Ahmad",
       specialty: "Head of Medical Clinics",
-      image: "https://images.unsplash.com/photo-1537368910025-700350fe46c7?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      image: "https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       quote: "Dedicated to providing comprehensive primary care and specialized medical consultations."
     },
     {
